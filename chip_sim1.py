19cc244121da666ffabd3d273042b46463caffa3
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-04-07 14:24:59 -0400

    new

---
+++ chip_sim1.py
@@ -0,0 +1,44 @@
+class Chipotle:
+    """Simulates a person ordering food at Chipotle
+    Attributes:
+        name(str): the person ordering
+    """
+    
+    def __init__(self, name):
+        """Initializes the customer object and set the name attribute to the name  sets on type of food offered like 
+        
+        Args:
+            name (str): the customer name
+        
+        Side effects:
+            Initializes self.name
+        """
+    
+    def customer_pref(self, bowl, burrito):
+        """customer determines what kind of burrito he wants narrowing his options for toppings
+        dictionary of option and prices
+        """
+    def calories_count(self):
+        """ determines the number of calories per order"
+        use dictionary
+        put in end of class key value pair, last
+        """
+    def customer_budget(self):
+        """customers can select id they have a budget and if the customer
+         surpasses the budget they will start the order again or can remove items, before price
+        """
+    def dietary(self, ):
+        """determines if the ingredients selected fit the restrictions
+        comparir order within the dietary restrictions, set
+        """
+    def extra_topings(self,guac, queso):
+        """takes into account additional toppings such as duplicates to the list
+        combine with next method"""
+    def sides_option( self, chips, drink):
+        """ asks for sides
+        """
+    def price_cal(self):
+        """determines the price of the customer order
+        """
+    def 
+        
