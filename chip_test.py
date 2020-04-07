7823fe5ee1edff22397d7e95bffc32c701f7599c
joelinoa <joelinoa@umd.edu>
2020-04-07 16:14:29 -0400

    edited tester

--- chip_test.py
+++ chip_test.py
@@ -2,10 +2,11 @@ import unittest
 import chip_sim1
 
 class TestChipotleSim(unittest.TestCase):
-"""Test chipotle simulator"""
+    """Test chipotle simulator"""
     def SetUp(self):
-"""Test whether SetUp works as expected"""
+        """Test whether SetUp works as expected"""
         self.order1 = chip_sim1.burrito({})
+        self.order2 = chip_sim1.Bowl({})
     def test_init(self):
         self.assertEqual(self.order1.name, "Gabby")
         self.assertNotEqual(self.order1.name, "5")
@@ -16,13 +17,13 @@ class TestChipotleSim(unittest.TestCase):
         self.assertNotEqual(self.order1.caloriescount, 0)
     def test_dietary(self):
         self.assertEqual("vegan, vegetarian")
-        self.assertNotEqual("vegan, vegeterian")
+        self.assertNotEqual("vegan, vegeterian")     
+    def test_tor_pref(self):
+        self.assertEqual(order2.tor_pref , True)   
     def test_extra(self):
-        
-    def test_sides_option(self):
-        
+        self.assertEqual(order1.extra[price], 5)   
     def test_price_cal(self):
-        
+        self.assertEqual(order1.price_cal, 15)
 
         
 if __name__ == "__main__":
