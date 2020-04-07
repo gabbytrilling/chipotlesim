b5a8474ce37c21527a9c57f6b3d6553c85d66db8
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-04-07 14:44:53 -0400

    new new

--- chip_sim1.py
+++ chip_sim1.py
@@ -1,3 +1,5 @@
+""" Simulates a person ordering food at Chipotle.
+"""
 class Chipotle:
     """Simulates a person ordering food at Chipotle
     Attributes:
@@ -27,11 +29,11 @@ class Chipotle:
         """customers can select id they have a budget and if the customer
          surpasses the budget they will start the order again or can remove items, before price
         """
-    def dietary(self, ):
+    def dietary(self, paleo, ):
         """determines if the ingredients selected fit the restrictions
         comparir order within the dietary restrictions, set
         """
-    def extra_topings(self,guac, queso):
+    def extra_topings(self,guac, queso,protein,chips):
         """takes into account additional toppings such as duplicates to the list
         combine with next method"""
     def sides_option( self, chips, drink):
@@ -40,5 +42,43 @@ class Chipotle:
     def price_cal(self):
         """determines the price of the customer order
         """
-    def 
         
+        
+""" Simulates a person ordering food at Chipotle.
+"""
+
+class Burrito:
+    """Simulates a person ordering food at Chipotle
+    Attributes:
+        name(str): the person ordering
+    """
+    def __init__(self, name):
+        """Initializes the customer object and set the name attribute to the name  sets on type of food offered like 
+        
+        Args:
+            name (str): the customer name
+        
+        Side effects:
+            Initializes self.name
+        """
+    
+    def dietary(self, paleo, ):
+        """determines if the ingredients selected fit the restrictions
+        comparir order within the dietary restrictions, set
+        """
+    def extra(self,guac, queso,protein,chips,drink):
+        """takes into account additional toppings such as duplicates to the list
+        combine with next method"""
+        
+     def customer_budget(self):
+        """customers can select id they have a budget and if the customer
+         surpasses the budget they will start the order again or can remove items, before price
+        """ 
+    def price_cal(self):
+        """determines the price of the customer order
+        """
+      
+class Bowl(Burrito):
+    
+if __name__ = "__main__":
+  
