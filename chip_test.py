1d771d5eb17f0371cb97aa416ca634c375244a05
Gabrielle <gtrillin@terpmail.umd.edu>
2020-04-07 16:00:55 -0400

    new update

--- chip_test.py
+++ chip_test.py
@@ -2,19 +2,28 @@ import unittest
 import chip_sim1
 
 class TestChipotleSim(unittest.TestCase):
+"""Test chipotle simulator"""
     def SetUp(self):
-        
+"""Test whether SetUp works as expected"""
+        self.order1 = chip_sim1.burrito({})
+    def test_init(self):
+        self.assertEqual(self.order1.name, "Gabby")
+        self.assertNotEqual(self.order1.name, "5")
     def test_customer_pref(self):
-    
+        self.assertEqual(self.order1.customer_pref, [])
     def test_calories_count(self):
-    
+        self.assertGreater(2,0)
+        self.assertNotEqual(self.order1.caloriescount, 0)
     def test_dietary(self):
-    
-    def test_extra_toppings(self):
-    
+        self.assertEqual("vegan, vegetarian")
+        self.assertNotEqual("vegan, vegeterian")
+    def test_extra(self):
+        
     def test_sides_option(self):
-    
+        
     def test_price_cal(self):
+        
+
         
 if __name__ == "__main__":
     unittest.main()
