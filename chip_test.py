1b457d83b699e324839327fd186ccd8ba590e2af
joelinoa <joelinoa@umd.edu>
2020-04-07 14:45:21 -0400

    unit testing

---
+++ chip_test.py
@@ -0,0 +1,20 @@
+import unittest
+import chip_sim1
+
+class TestChipotleSim(unittest.TestCase):
+    def SetUp(self):
+        
+    def test_customer_pref(self):
+    
+    def test_calories_count(self):
+    
+    def test_dietary(self):
+    
+    def test_extra_toppings(self):
+    
+    def test_sides_option(self):
+    
+    def test_price_cal(self):
+        
+if __name__ == "__main__":
+    unittest.main()
