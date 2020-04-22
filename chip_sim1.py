445cc9b123c8ce0dc84f4e38a1d05703b769223b
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-04-22 15:05:20 -0400

    worked on first 3 methods in the first class

--- chip_sim1.py
+++ chip_sim1.py
@@ -19,15 +19,22 @@ class Burrito:
         Side effects:
             Initializes self.name
         """
+        self.name = name
+        self.ingredients = []
         
-    def customer_pref(self, type):
+    def customer_pref(self, type1):
         """ Identifys wheather the customer wants a bowl or burrito.
         
         Args:
             type (str): determines whether the customer wants a bowl or burrito
              
         """
-    def dietary(self, type ):
+        if type1 != "bowl" or type1 != "burrito":
+            raise TypeError("This is not a valid option")
+        else:
+            return type1
+
+    def dietary(self, type1 = None ):
         """ Determines if the ingredients selected fit the restrictions
         comparir order within the dietary restrictions
         
@@ -38,6 +45,44 @@ class Burrito:
             retuns a dictionary of list: price, calorie count,
             and contents of the bowl.
         """
+        
+        keto_salad_bowl = ["supergreens lettuce blend", "steak",
+                            "tomatillo-red chili salsa", "cheese", "guacamole"]
+        vegan_bowl = ["brown rice", "sofritas", "black beans", 
+                      "fresh tomatillo salsa", "roasted chili- corn salsa", 
+                      "shredded romaine lettuce"]
+        vegetarian_bowl = ["super greens lettuce blend", "brown rice", 
+                           "black beans", "fajita veggies", 
+                           "fresh tomato salsa and guacamole"]
+        if type1 == "keto":
+            self.ingredients += keto_salad_bowl
+        elif type1 == "vegan":
+            self.ingredients += vegan_bowl
+        elif type1 == "vegetarian":
+            self.ingredients += vegetarian_bowl
+        else:
+            self.assemble()
+        return self.ingredients
+    
+    def assemble (self):
+        base = input("Brown Rice, White Rice, or Lettuce? ")
+        self.ingredients.append(base)
+        beans = input("pinto or black")
+        self.ingredients.append(beans)
+        protein = input("chicken, steak, carnitas, sofritas (vegan),"
+                        "veggies (vegan), barbacoa")
+        protein = protein.strip().split(",")
+        self.ingredients += protien
+        salsa = input("red chili salsa, tomatillo salsa, corn salsa,"
+                       "green chili salsa")
+        salsa = salsa.strip().split(",")
+        self.ingredients += salsa
+        toppings = input("cheese, guacamole, lettuce, sour cream, corn chips")
+        toppings = toppings.strip().split(",")
+        self.ingredients += toppings
+        
+        
+            
         
     def extra(self, type, amount):
         """ Takes into account additional toppings such as duplicates 
