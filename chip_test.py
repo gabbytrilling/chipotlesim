79f91c631796c380efdf09dc4a036968b3f54c52
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-05-05 14:21:07 -0400

    worked on some edits 2

--- chip_test.py
+++ chip_test.py
@@ -3,21 +3,32 @@ import chip_sim1
 
 class TestChipotleSim(unittest.TestCase):
     """Test chipotle simulator"""
-    def SetUp(self):
+    def setUp(self):
         """Test whether SetUp works as expected"""
-        self.order1 = chip_sim1.burrito({})
-        self.order2 = chip_sim1.Bowl({})
+        self.order1 = chip_sim1.Burrito("Gabby")
+        self.order2 = chip_sim1.Bowl("Joe")
     def test_init(self):
         self.assertEqual(self.order1.name, "Gabby")
-        self.assertNotEqual(self.order1.name, "5")
+        self.assertEqual(self.ingredients, [])
+        self.assertEqual(self.order, {"Name": "Gabby"})
     def test_customer_pref(self):
         self.assertEqual(self.order1.customer_pref, [])
     def test_calories_count(self):
         self.assertGreater(2,0)
         self.assertNotEqual(self.order1.caloriescount, 0)
     def test_dietary(self):
-        self.assertEqual("vegan, vegetarian")
-        self.assertNotEqual("vegan, vegeterian")     
+        self.order1.dietary("keto")
+        self.assertEqual(["lettuce", "steak","red chili salsa", "cheese",
+                          "guacamole"], self.order1.ingredients)
+        self.order1.dietary("vegan")
+        self.assertEqual(["brown rice", "sofritas", "black beans", 
+                      "tomatillo salsa", "corn salsa", 
+                      "lettuce"], self.order1.ingredients)
+        self.order1.dietary("vegetarian")
+        self.assertEqual(["lettuce", "brown rice", "black beans", "fajitas", 
+                           "tomato salsa","guacamole"], self.order1.ingredients)
+        #self.assertEqual("vegan, vegetarian")
+        #self.assertNotEqual("vegan, vegeterian")     
     def test_tor_pref(self):
         self.assertEqual(order2.tor_pref , True)   
     def test_extra(self):
