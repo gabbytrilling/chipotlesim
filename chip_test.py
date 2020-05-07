2eeb3e3fcac819683751d8e6fc80a5912c765806
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-05-07 14:14:03 -0400

    worked on some edits

--- chip_test.py
+++ chip_test.py
@@ -13,24 +13,29 @@ class TestChipotleSim(unittest.TestCase):
         self.assertEqual(self.order, {"Name": "Gabby"})
     def test_customer_pref(self):
         self.assertEqual(self.order1.customer_pref, [])
+        #added test code bellow
+        self.oreder1.customer_pref("bowl")
+        self.assertEquals("bowl")
+        self.order1.customer_pref("burrito")
+        self.assertEquals("burrito")
     def test_calories_count(self):
         self.assertGreater(2,0)
         self.assertNotEqual(self.order1.caloriescount, 0)
     def test_dietary(self):
         self.order1.dietary("keto")
-        self.assertEqual(["lettuce", "steak","red chili salsa", "cheese",
+        self.assertEquals(["lettuce", "steak","red chili salsa", "cheese",
                           "guacamole"], self.order1.ingredients)
         self.order1.dietary("vegan")
-        self.assertEqual(["brown rice", "sofritas", "black beans", 
+        self.assertEquals(["brown rice", "sofritas", "black beans", 
                       "tomatillo salsa", "corn salsa", 
                       "lettuce"], self.order1.ingredients)
         self.order1.dietary("vegetarian")
-        self.assertEqual(["lettuce", "brown rice", "black beans", "fajitas", 
+        self.assertEquals(["lettuce", "brown rice", "black beans", "fajitas", 
                            "tomato salsa","guacamole"], self.order1.ingredients)
         #self.assertEqual("vegan, vegetarian")
         #self.assertNotEqual("vegan, vegeterian")     
     def test_tor_pref(self):
-        self.assertEqual(order2.tor_pref , True)   
+        self.assertEqual(order1.tor_pref , True)   
     def test_extra(self):
         self.assertEqual(order1.extra[price], 5)   
     def test_price_cal(self):
