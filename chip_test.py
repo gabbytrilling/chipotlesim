78b39e973a43fbd98d6b011924b8bc6b38b1b551
joelinoa <joelinoa@umd.edu>
2020-05-14 16:56:52 -0400

    fixed test code

--- chip_test.py
+++ chip_test.py
@@ -11,24 +11,15 @@ class TestChipotleSim(unittest.TestCase):
         self.order4 = chip_sim1.Burrito("john")
     def test_init(self):
         self.assertEqual(self.order1.name, "gabby")
-        self.assertEqual(self.ingredients, [])
-        self.assertEqual(self.order, {"Name": "gabby"})
+        self.assertEqual(self.order1.ingredients, [])
     def test_customer_pref(self):
-        self.assertEqual(self.order1.customer_pref, [])
-        #added test code bellow
-        self.order2.customer_pref("bowl")
-        self.assertEqual(self.order2.type1, "bowl")
-        self.order1.customer_pref("burrito")
-        self.assertEqual(self.order1.type1,"burrito")
-        self.order3.customer_pref("bowl")
-        self.assertEqual(self.order3.type1, "bowl")
-        self.order4.customer_pref("burrito")
-        self.assertEqual(self.order4.type1,"burrito")
+        self.assertEqual(chip_sim1.customer_pref("bowl"), 'bowl')
+        self.assertEqual(chip_sim1.customer_pref('burrito'), 'burrito')
     def test_calories_count(self):
-        self.assertEqual(order1.calories_count, 525)
-        self.assertEqual(order2.calories_count, 725)
-        self.assertEqual(order3.calories_count, 850)
-        self.assertEqual(order4.calories_count, 755)
+        self.assertEqual(self.order1.calories_count, 525)
+        self.assertEqual(self.order2.calories_count, 725)
+        self.assertEqual(self.order3.calories_count, 850)
+        self.assertEqual(self.order4.calories_count, 755)
         
         self.assertNotEqual(self.order1.calories_count, 0)
     def test_dietary(self):
@@ -46,13 +37,9 @@ class TestChipotleSim(unittest.TestCase):
         self.assertEqual([], self.order4.ingredients)
         #self.assertEqual("vegan, vegetarian")
         #self.assertNotEqual("vegan, vegeterian") 
-    def test_assemble(self):
-        self.order4.assemble(["brown rice","pinto", "steak", "corn salsa",
-                              "lettuce"], ingredients)
-            
-    def test_tor_pref(self):
-        self.assertEqual(order2.tor_pref.option, True)
-        self.assertEqual(order3.tor_pref.option, False)  
+    def test_tortilla(self):
+        self.assertEqual(self.order2.tortilla('yes'), True)
+        self.assertEqual(self.order3.tortilla('no'), False)  
     def test_extra(self):
         self.order1.extra("none")
         self.assertEquals(["lettuce", "steak","red chili salsa", "cheese",
@@ -61,21 +48,21 @@ class TestChipotleSim(unittest.TestCase):
         self.assertEquals(["brown rice", "sofritas", "black beans", 
                       "tomatillo salsa", "corn salsa", 
                       "lettuce", "sofritas"], self.order2.ingredients)
-        self.assertEqual(order2.extra, 10.5)
+        self.assertEqual(self.order2.extra, 10.5)
         self.order3.extra("guacamole")
         self.assertEquals(["lettuce", "brown rice", "black beans", "fajitas", 
                            "tomato salsa","guacamole", "guacamole"], self.order3.ingredients)
-        self.assertEqual(order3.extra, 10)
+        self.assertEqual(self.order3.extra, 10)
         self.order4.extra("chicken")
         self.assertEquals(["brown rice","pinto", "steak", "corn salsa",
                             "lettuce", "chicken"], self.order4.ingredients)
-        self.assertEqual(order4.extra, 11.5)
+        self.assertEqual(self.order4.extra, 11.5)
         #self.assertEqual(order1.extra[price], 5)   
     def test_price_cal(self):
-        self.assertEqual(order1.price_cal, 8.7)
-        self.assertEqual(order2.price_cal, 7.70)
-        self.assertEqual(order3.price_cal, 7.70)
-        self.assertEqual(order4.price_cal, 8.7)
+        self.assertEqual(self.order1.price_cal(), 8.7)
+        self.assertEqual(self.order2.price_cal(), 7.70)
+        self.assertEqual(self.order3.price_cal(), 7.70)
+        self.assertEqual(self.order4.price_cal(), 8.7)
 
         
 if __name__ == "__main__":
