1c41ff4ff570e5475ec382b8c573f64d0fdba6d4
joelinoa <joelinoa@umd.edu>
2020-05-14 18:01:47 -0400

    new test

--- chip_test.py
+++ chip_test.py
@@ -8,21 +8,25 @@ class TestChipotleSim(unittest.TestCase):
         self.order1 = chip_sim1.Burrito("gabby")
         self.order2 = chip_sim1.Bowl("joe")
         self.order3 = chip_sim1.Bowl("bob")
-        self.order4 = chip_sim1.Burrito("john")
     def test_init(self):
+        """Test whether init works as expected"""
         self.assertEqual(self.order1.name, "gabby")
         self.assertEqual(self.order1.ingredients, [])
     def test_customer_pref(self):
+        """Test whether customer_pref works as expected"""
         self.assertEqual(chip_sim1.customer_pref("bowl"), 'bowl')
         self.assertEqual(chip_sim1.customer_pref('burrito'), 'burrito')
     def test_calories_count(self):
-        self.assertEqual(self.order1.calories_count, 525)
-        self.assertEqual(self.order2.calories_count, 725)
-        self.assertEqual(self.order3.calories_count, 850)
-        self.assertEqual(self.order4.calories_count, 755)
-        
-        self.assertNotEqual(self.order1.calories_count, 0)
+        """Tests if calories_count works as expected"""
+        self.order1.dietary("keto")
+        self.assertEqual(self.order1.calories_count(), 525)
+        self.order2.dietary("vegan")
+        self.assertEqual(self.order2.calories_count(), 575)
+        self.order3.dietary("vegetarian")
+        self.assertEqual(self.order3.calories_count(), 620)
+        self.assertNotEqual(self.order1.calories_count(), 0)
     def test_dietary(self):
+        """Tests whether dietary works as expected"""
         self.order1.dietary("keto")
         self.assertEquals(["lettuce", "steak","red chili salsa", "cheese",
                           "guacamole"], self.order1.ingredients)
@@ -31,38 +35,30 @@ class TestChipotleSim(unittest.TestCase):
                       "tomatillo salsa", "corn salsa", 
                       "lettuce"], self.order2.ingredients)
         self.order3.dietary("vegetarian")
-        self.assertEquals(["lettuce", "brown rice", "black beans", "fajitas", 
-                           "tomato salsa","guacamole"], self.order3.ingredients)
-        self.order4.dietary("None")
-        self.assertEqual([], self.order4.ingredients)
-        #self.assertEqual("vegan, vegetarian")
-        #self.assertNotEqual("vegan, vegeterian") 
+        self.assertEquals(['veggie',"lettuce", "brown rice", "black beans",
+                           "fajitas", "tomato salsa","guacamole"],
+                           self.order3.ingredients)
     def test_tortilla(self):
+        """Test whether tortilla works as expected"""
         self.assertEqual(self.order2.tortilla('yes'), True)
         self.assertEqual(self.order3.tortilla('no'), False)  
     def test_extra(self):
-        self.order1.extra("none")
-        self.assertEquals(["lettuce", "steak","red chili salsa", "cheese",
-                          "guacamole"], self.order1.ingredients)
-        self.order2.extra("sofritas")
-        self.assertEquals(["brown rice", "sofritas", "black beans", 
-                      "tomatillo salsa", "corn salsa", 
-                      "lettuce", "sofritas"], self.order2.ingredients)
-        self.assertEqual(self.order2.extra, 10.5)
-        self.order3.extra("guacamole")
-        self.assertEquals(["lettuce", "brown rice", "black beans", "fajitas", 
-                           "tomato salsa","guacamole", "guacamole"], self.order3.ingredients)
-        self.assertEqual(self.order3.extra, 10)
-        self.order4.extra("chicken")
-        self.assertEquals(["brown rice","pinto", "steak", "corn salsa",
-                            "lettuce", "chicken"], self.order4.ingredients)
-        self.assertEqual(self.order4.extra, 11.5)
-        #self.assertEqual(order1.extra[price], 5)   
+        """Tests whether extra works as expected"""
+        self.order1.dietary("keto")
+        self.assertEqual(self.order1.extra(), 8.70)
+        self.order2.dietary("vegan")
+        order2 = self.order2.extra(['guacamole'])
+        self.assertEqual(order2, 10.0)
+        self.order3.dietary("vegetarian")
+        self.assertEqual(self.order3.extra(['chicken']), 10.5)
     def test_price_cal(self):
+        """Tests whether price_cal works as expected"""
+        self.order1.dietary('keto')
         self.assertEqual(self.order1.price_cal(), 8.7)
+        self.order2.dietary('vegan')
         self.assertEqual(self.order2.price_cal(), 7.70)
+        self.order3.dietary('vegetarian')
         self.assertEqual(self.order3.price_cal(), 7.70)
-        self.assertEqual(self.order4.price_cal(), 8.7)
 
         
 if __name__ == "__main__":
